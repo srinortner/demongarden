5629456a3a69430b7121049e1156171e3ceafd4f
Susi <e1526772@student.tuwien.ac.at>
2021-11-30 20:30:12 +0100

    water plant on Q

---
+++ Assets/Scripts/PlantBoxCollision.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlantBoxCollision : MonoBehaviour
+{
+    public Text text;
+
+    private bool playerClose;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerClose && Input.GetKeyDown(KeyCode.Q))
+        {
+            waterPlant();
+            print("water plant");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+        if (other.tag.Equals("Player"))
+        {
+            text.enabled = true;
+            playerClose = true;
+            
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            text.enabled = false;
+            playerClose = false;
+        }
+    }
+
+    private void waterPlant()
+    {
+        GetComponent<Renderer>().material.SetColor("_Color", new Color(0.2f,0.15f,0.09f,1.0f));
+    }
+}
