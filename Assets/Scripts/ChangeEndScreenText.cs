fc9dba117fefeeca743fa6da692057004296e58c
Viktoria-1 <e1526186@student.tuwien.ac.at>
2022-01-24 18:22:42 +0100

    Hotfix endscreen update for winning
    Hotfix endscreen update for winning

--- Assets/Scripts/ChangeEndScreenText.cs
+++ Assets/Scripts/ChangeEndScreenText.cs
@@ -8,17 +8,28 @@ public class ChangeEndScreenText : MonoBehaviour
 {
     private string secondsSurvived;
     public Text secondsText;
-    public Text won;
+    public TextMeshProUGUI won;
+    //for win screen
+    public TextMeshProUGUI text1;
+    public TextMeshProUGUI text2;
 
     public void Start()
     {
+        text1.enabled = true;
+        text2.enabled = true;
+        secondsText.enabled = true;
         secondsSurvived = CrossSceneInformation.secondsSurvived;
+
      //   print("Seconds Survived " + secondsSurvived);
     //    secondsText = GameObject.FindWithTag("menutext").GetComponent<TextMeshPro>();
         secondsText.text = secondsSurvived;
         if (CrossSceneInformation.won)
         {
+            text1.enabled = false;
+            text2.enabled = false;
+            secondsText.enabled = false;
             won.text = "You won!";
+            CrossSceneInformation.won = false;
         }
     }
 }
